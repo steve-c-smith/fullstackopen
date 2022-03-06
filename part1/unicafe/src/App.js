065035a12080695b440e988d1115af95d2fe3daa
Steve <stevecsmith92@gmail.com>
2022-03-05 23:16:28 -0600

    1.10-1.11 Completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 
-const StatisticsDisplay = ({ label, value }) => <div>{label} {value}</div>
+const StatisticsLine = ({ label, value }) => {
+  return (
+    <tr>
+      <td>{label}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({ isFeedbackProvided, neutral, bad, good }) => {
   const getTotal = () => good + neutral + bad
@@ -15,20 +22,22 @@ const Statistics = ({ isFeedbackProvided, neutral, bad, good }) => {
   
   if(isFeedbackProvided){
     return (
-      <div>
-        <StatisticsDisplay label="good" value={good} />
-        <StatisticsDisplay label="neutral" value={neutral} />
-        <StatisticsDisplay label="bad" value={bad} />
-        <StatisticsDisplay label="all" value={getTotal()} />
-        <StatisticsDisplay label="average" value={getAverage().toFixed(2)} />
-        <StatisticsDisplay label="positive" value={getPercentPositive().toFixed(2) + "%"} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticsLine label="good" value={good} />
+          <StatisticsLine label="neutral" value={neutral} />
+          <StatisticsLine label="bad" value={bad} />
+          <StatisticsLine label="all" value={getTotal()} />
+          <StatisticsLine label="average" value={getAverage().toFixed(2)} />
+          <StatisticsLine label="positive" value={getPercentPositive().toFixed(2) + "%"} />
+        </tbody>
+      </table>
     )
   }
   return <p>No Feedback Provided</p>
 }
 
-const FeedbackButton = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const Header = (props) => <h1>{props.text}</h1>
 
 const App = () => {
@@ -61,9 +70,9 @@ const App = () => {
   return (
     <div>
         <Header text="give feedback" />
-        <FeedbackButton onClick={handleFeedback("good")} text="good" />
-        <FeedbackButton onClick={handleFeedback("neutral")} text="neutral" />
-        <FeedbackButton onClick={handleFeedback("bad")} text="bad" />
+        <Button onClick={handleFeedback("good")} text="good" />
+        <Button onClick={handleFeedback("neutral")} text="neutral" />
+        <Button onClick={handleFeedback("bad")} text="bad" />
         <Header text="statistics" />
         <Statistics isFeedbackProvided={isFeedbackProvided} good={good} neutral={neutral} bad={bad} />
     </div>
