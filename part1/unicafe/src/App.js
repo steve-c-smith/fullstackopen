a3cdc7dede0aa5641001523a2b89933270064934
Steve <stevecsmith92@gmail.com>
2022-03-05 20:18:24 -0600

    1.6 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+const FeedbackDisplay = ({ label, value }) => <div>{label} {value}</div>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Header = (props) => <h1>{props.text}</h1>
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header text="give feedback" />
+        <Button onClick={() => {setGood(good + 1)}} text="good" />
+        <Button onClick={() => {setNeutral(neutral + 1)}} text="neutral" />
+        <Button onClick={() => {setBad(bad + 1)}} text="bad" />
+        <Header text="statistics" />
+        <FeedbackDisplay label="good" value={good} />
+        <FeedbackDisplay label="neutral" value={neutral} />
+        <FeedbackDisplay label="bad" value={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
